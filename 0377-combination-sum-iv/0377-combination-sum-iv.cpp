d8f0431001902ee855986f3e740ae1e33ee5db21
Saksham Gupta <76028123+sakshgupta@users.noreply.github.com>
2023-07-05 18:58:56 +0530

    Time: 0 ms (100%), Space: 6.7 MB (45.13%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // memo
     int solve(vector<int> &arr, int t, vector<int> &dp){
         if(t == 0) return 1;
         if(t < 0) return 0;
@@ -13,8 +14,25 @@ public:
 
         return dp[t] = takeIt;
     }
+
+    // tab
+    int solve_tab(vector<int> &arr, int t){
+        uint32_t* dp = new uint32_t[t+1](); // did this so as to fit 32 bit integer
+
+        dp[0] = 1;
+
+        for(int i=1; i<=t; i++){
+            for(int j=0; j<arr.size(); j++){
+                if(i - arr[j] >= 0){
+                    dp[i] += dp[i - arr[j]];
+                }
+            }
+        }
+
+        return dp[t];
+    }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1, -1);
-        return solve(nums, target, dp);
+        // vector<int> dp(target+1, -1);
+        return solve_tab(nums, target);
     }
 };
