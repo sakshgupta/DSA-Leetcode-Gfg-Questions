3e81de457420f387b0998a931e83f94585536827
Saksham Gupta <76028123+sakshgupta@users.noreply.github.com>
2023-07-21 16:27:02 +0530

    Time: 256 ms (24.98%) | Memory: 87.4 MB (30.28%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    vector<int> nextSmaller(vector<int> &arr)
+    {
+        int n = arr.size();
+        vector<int> v(n);
+        stack<int> s;
+        s.push(-1);
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (s.empty() == false and s.top() != -1 and arr[i] <= arr[s.top()])
+            {
+                s.pop();
+            }
+            v[i] = s.top();
+            s.push(i);
+        }
+        return v;
+    }
+    vector<int> prevSmaller(vector<int> &arr){
+        int n = arr.size();
+        vector<int> v(n);
+        stack<int> s;
+        s.push(-1);
+
+        for (int i = 0; i < n; i++)
+        {
+            while (s.empty() == false and s.top() != -1 and arr[i] <= arr[s.top()])
+            {
+                s.pop();
+            }
+            v[i] = s.top();
+            s.push(i);
+        }
+        return v;
+    }
+
+    int solve(vector<int> &arr){
+        // idea: is to find the next smaller and prev smaller ele for each index 
+        // then cal area by subtracting nextSmaller - prevSmaller - 1
+        // as in area we can only include the bars which are greater than the current
+        int n = arr.size();
+
+        vector<int> next(n);
+        next = nextSmaller(arr);
+
+        vector<int> prev(n);
+        prev = prevSmaller(arr);
+
+        int area = INT_MIN;
+
+        for(int i=0; i<n; i++){
+            int length = arr[i];
+
+            // if their is no next smaller element
+            if(next[i] == -1) next[i] = n;
+
+            int breadth = next[i] - prev[i] - 1;
+
+            int currarea = length * (breadth);
+            area = max(area, currarea);
+        }
+
+        return area;
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        return solve(heights);
+    }
+};
