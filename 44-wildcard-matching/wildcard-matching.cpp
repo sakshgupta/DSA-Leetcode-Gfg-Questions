914e3b7b51e20259aba7992bf606a859d01bc658
Saksham Gupta <76028123+sakshgupta@users.noreply.github.com>
2023-07-21 19:20:44 +0530

    Time: 100 ms (31.09%) | Memory: 27.8 MB (22.80%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     // memo
     bool solve(string &s, string &p, int i, int j, vector<vector<int>> &dp){
-        if(i < 0 and j < 0) return true; // string and pat both finished
-        if(i >= 0 and j < 0) return false; // string not finish, pat finished
-        if(i < 0 and j >= 0) { // string finished, pat not finished
+        if(i == 0 and j == 0) return true; // string and pat both finished
+        if(i > 0 and j == 0) return false; // string not finish, pat finished
+        if(i == 0 and j > 0) { // string finished, pat not finished
             // two case:
             // s = a b c, p = a * a b c
-            for(int k = 0; k<=j; k++){
-                if(p[k] != '*') return false;
+            for(int k = 1; k<=j; k++){
+                if(p[k-1] != '*') return false;
             }
             // s = a b c, p = * * a b c
             return true; 
@@ -17,8 +17,8 @@ public:
         if(dp[i][j] != -1) return dp[i][j];
 
         // if match or ?
-        if(s[i] == p[j] or p[j] == '?') return dp[i][j] = solve(s, p, i-1, j-1, dp);
-        else if(p[j] == '*'){
+        if(s[i-1] == p[j-1] or p[j-1] == '?') return dp[i][j] = solve(s, p, i-1, j-1, dp);
+        else if(p[j-1] == '*'){
             // two case
             // * = empty string
             int op1 = solve(s, p, i, j-1, dp);
@@ -29,8 +29,39 @@ public:
         }
         else return dp[i][j] = false; // not match
     }
+
+    // tab
+    bool solve_tab(string &s, string &p){
+        vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, 0));
+        dp[s.size()][p.size()] = 1;
+        for(int i=p.size()-1; i>=0; i--){
+            if(p[i] == '*'){
+                dp[s.size()][i] = dp[s.size()][i+1];
+            }
+            else{
+                dp[s.size()][i] = 0;
+            }
+        }
+
+        for(int i=s.size()-1; i>=0; i--){
+            for(int j=p.size()-1; j>=0; j--){
+                bool ans = 0;
+                if(p[j] == '?' || s[i] == p[j]){
+                    ans = dp[i+1][j+1];
+                }
+                else if(p[j] == '*'){
+                    ans = dp[i][j+1] || dp[i+1][j];
+                }
+
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[0][0];
+    }
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size()+1, vector<int> (p.size()+1, -1));
-        return solve(s, p, s.length()-1, p.length()-1, dp);
+        // vector<vector<int>> dp(s.size()+1, vector<int> (p.size()+1, -1));
+        // return solve(s, p, s.length(), p.length(), dp);
+        return solve_tab(s, p);
     }
 };
