8729bde67125394f458324bcaceac77d9a18141c
Saksham Gupta <76028123+sakshgupta@users.noreply.github.com>
2023-07-22 12:34:41 +0530

    Time: 99 ms (98.93%) | Memory: 67.7 MB (34.29%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -10,8 +10,33 @@ public:
             return dp[i][mustPick] = max(0, A[i] + solve(A, i+1, true, dp)); // either stop here or choose current element and recurse
         return dp[i][mustPick] = max(solve(A, i+1, false, dp), A[i] + solve(A, i+1, true, dp)); // try both choosing current element or not choosing
     }
+
+    // tab
+    int solve_tab(vector<int>& arr) {
+        vector<vector<int>> dp(2, vector<int>(size(arr)));
+
+        dp[0][0] = dp[1][0] = arr[0];
+
+        for(int i = 1; i < size(arr); i++) {
+            dp[1][i] = max(arr[i], arr[i] + dp[1][i-1]);
+            dp[0][i] = max(dp[0][i-1], dp[1][i]);
+        }
+        return dp[0].back();
+    }
+
+    // kadane
+    int solve_kadane(vector<int>& arr) {
+        int curMax = 0, maxTillNow = INT_MIN;
+        for(auto c : arr)
+            curMax = max(c, curMax + c),
+            maxTillNow = max(maxTillNow, curMax);
+        return maxTillNow;
+    }
+
     int maxSubArray(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size()+1, vector<int>(3, -1));
-        return solve(nums, 0, false, dp);
+        // vector<vector<int>> dp(nums.size()+1, vector<int>(3, -1));
+        // return solve(nums, 0, false, dp);
+
+        return solve_kadane(nums);
     }
 };
